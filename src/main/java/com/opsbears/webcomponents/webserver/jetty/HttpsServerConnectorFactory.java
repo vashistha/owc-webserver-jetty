2f800c876741e928fa6ed86a0a65e0a00bcba612
Janos Pasztor <janos@pasztor.at>
2017-11-19 15:21:56 +0100

    Restored stock server for testing

--- src/main/java/com/opsbears/webcomponents/webserver/jetty/HttpsServerConnectorFactory.java
+++ src/main/java/com/opsbears/webcomponents/webserver/jetty/HttpsServerConnectorFactory.java
@@ -2,6 +2,10 @@ package com.opsbears.webcomponents.webserver.jetty;
 
 import com.opsbears.webcomponents.net.IPAddressPortPair;
 import com.opsbears.webcomponents.webserver.X509CertificateProvider;
+import org.eclipse.jetty.alpn.ALPN;
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
+import org.eclipse.jetty.http2.HTTP2Cipher;
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
@@ -13,27 +17,43 @@ public class HttpsServerConnectorFactory implements ServerConnectorFactory {
     private final IPAddressPortPair ipPortPair;
     private final Collection<X509CertificateProvider> certificateProviders;
 
-    public HttpsServerConnectorFactory(IPAddressPortPair ipPortPair, Collection<X509CertificateProvider> certificateProviders) {
+    public HttpsServerConnectorFactory(
+        IPAddressPortPair ipPortPair,
+        Collection<X509CertificateProvider> certificateProviders
+    ) {
         this.ipPortPair = ipPortPair;
         this.certificateProviders = certificateProviders;
     }
 
     @Override
     public ServerConnector create(Server server) {
-        HttpConfiguration https = new HttpConfiguration();
-        https.setSendServerVersion(false);
-        https.setSendXPoweredBy(false);
-        https.addCustomizer(new SecureRequestCustomizer());
-        SslContextFactory sslContextFactory = new ExternalCertificateSslContextFactory(certificateProviders);
-        ServerConnector httpsConnector = new ServerConnector(
-            server,
-            new SslConnectionFactory(sslContextFactory, "http/1.1"),
-            new HttpConnectionFactory(https)
-        );
+        HttpConfiguration httpConfiguration = new HttpConfiguration();
+        httpConfiguration.setSendServerVersion(false);
+        httpConfiguration.setSendXPoweredBy(false);
+        httpConfiguration.addCustomizer(new SecureRequestCustomizer());
 
+        HttpConnectionFactory https = new HttpConnectionFactory(httpConfiguration);
+        SslContextFactory sslContextFactory = new ExternalCertificateSslContextFactory(certificateProviders);
+        sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
+        sslContextFactory.setUseCipherSuitesOrder(true);
+
+        ServerConnector httpsConnector;
+        try {
+            ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+            alpn.setDefaultProtocol("h2");
+            HTTP2ServerConnectionFactory h2 = new HTTP2ServerConnectionFactory(httpConfiguration);
+            SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory, alpn.getProtocol());
+            httpsConnector = new ServerConnector(server, ssl, alpn, h2, https);
+        } catch (IllegalStateException e) {
+            //No ALPN
+            SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory, https.getProtocol());
+            httpsConnector = new ServerConnector(server, ssl, https);
+        }
         httpsConnector.setHost(ipPortPair.getIpAddress().toString());
         httpsConnector.setPort(ipPortPair.getPort());
         httpsConnector.setIdleTimeout(30000);
+
+        ALPN.debug = true;
         return httpsConnector;
     }
 }
---
+++ src/test/java/StockServerApplication.java
@@ -0,0 +1,28 @@
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class StockServerApplication {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(9999);
+        HttpConfiguration https = new HttpConfiguration();
+        https.addCustomizer(new SecureRequestCustomizer());
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(StockServerApplication.class.getResource(
+            "/keystore.jks").toExternalForm());
+        sslContextFactory.setKeyStorePassword("12345678");
+        sslContextFactory.setKeyManagerPassword("12345678");
+        ServerConnector sslConnector = new ServerConnector(server,
+            new SslConnectionFactory(sslContextFactory, "http/1.1"),
+            new HttpConnectionFactory(https));
+        sslConnector.setPort(9998);
+        server.setConnectors(new Connector[] { connector, sslConnector });
+
+        server.start();
+        server.join();
+    }
+}
